a04007c49ccd6aee2f79c7c4fd5319fd020e8b18
alex-shen1 <shen.alexander@gmail.com>
2021-03-19 17:46:08 -0400

    Created prefabs holding player with basic controls and enemy with basic AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
     public float forceAmount = 3;
     public GameObject player;
