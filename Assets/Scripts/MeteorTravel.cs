f89b97fd440d794b0ce291d77d333d82c1c6a531
ianharvey302 <iharv1@verizon.net>
2021-03-19 18:07:24 -0400

    Meteor base code

---
+++ Assets/Scripts/MeteorTravel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorTravel : MonoBehaviour
+{
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 0.25)
+            GetComponent<CircleCollider2D>().enabled = true;
+        if (timer <= 0.0)
+            Delete();
+        timer -= Time.deltaTime;
+    }
+
+    void Delete()
+    {
+        Destroy(this.gameObject);
+    }
+}
