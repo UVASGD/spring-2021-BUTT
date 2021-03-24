b061bd4a0cb0e1559c850b39aa655bfb1cc02118
Jav <jt6xnk@virginia.edu>
2021-03-24 18:14:13 -0400

    Enemy movement on beat

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -17,17 +17,34 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
+    }
+
+    void OnBeat(int beatNum) { 
         Vector2 forceToApply;
 
-        Transform enemyTransform =  this.gameObject.GetComponent<Transform>();
+        Transform enemyTransform = this.gameObject.GetComponent<Transform>();
         Transform playerTransform = player.GetComponent<Transform>();
         // operator override means we can just subtract directly
-        Vector3 vecToPlayer = playerTransform.position - enemyTransform.position;
-        // don't care about Z difference
+         Vector3 vecToPlayer = playerTransform.position - enemyTransform.position;
+         // don't care about Z difference
         forceToApply = new Vector2(vecToPlayer.x, vecToPlayer.y);
-        forceToApply.Normalize();
-        forceToApply = forceToApply * 3;
+         forceToApply.Normalize();
+         forceToApply = forceToApply* forceAmount;
 
-        rb.AddForce(forceToApply);
+        if (beatNum % 2 == 1)
+        {
+
+            rb.AddForce(forceToApply, ForceMode2D.Impulse);
+            rb.drag = 50;
+
+        
+        }
+       
+        
+       //forceToApply.Normalize();
+        //forceToApply = forceToApply * 3;
+
+        //rb.AddForce(forceToApply);
     }
 }
