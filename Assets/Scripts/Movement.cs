d685a945f3e7fb6c6eaadf2342510f686474cdef
ianharvey302 <iharv1@verizon.net>
2021-04-07 18:02:57 -0400

    Teleport code added to movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,10 +7,12 @@ public class Movement : MonoBehaviour
     public float forceAmount = 100;
 
     private Rigidbody2D rb;
+    private Transform tf;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        tf = GetComponent<Transform>();
     }
 
 
@@ -33,6 +35,12 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(new Vector2(forceAmount,0));
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Vector3 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            tf.position = new Vector3(temp.x, temp.y, 0);
+            rb.velocity = new Vector3(0, 0, 0);
         }
     }
 }
