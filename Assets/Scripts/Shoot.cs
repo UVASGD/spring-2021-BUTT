628e649c588b5e870af3c5c131c6d120c1058e51
Jav <jt6xnk@virginia.edu>
2021-04-25 20:46:38 -0400

    lol

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -13,6 +13,7 @@ public class Shoot : MonoBehaviour
     public float laserDuration = .1F;
     public Rigidbody2D bullet;
     public bool shootEnabled = true;
+
     LineRenderer laser;
     float lastLaserFire = -1;
     // Start is called before the first frame update
@@ -32,9 +33,28 @@ public class Shoot : MonoBehaviour
             {
                 laser.SetPositions(new Vector3[] { Vector3.zero, Vector3.zero });
             }
-            if (Input.GetMouseButtonDown(0) && GetComponent<Parry>().ammo > 0 && musicManager.RateAction() != ActionRating.INVALID)
+            if (Input.GetMouseButtonDown(0) && GetComponent<Parry>().ammo > 0)
             {
-                FireLaser();
+                ActionRating ar = musicManager.RateAction();
+                if (ar != ActionRating.INVALID)
+                {
+                    
+                    switch (ar)
+                    {
+                        case ActionRating.GOOD:
+                            FireLaserBoostGood();
+                            break;
+                        case ActionRating.PERFECT:
+                            FireLaserBoostPerfect();
+                            break;
+                        default:
+                            FireLaser();
+                            break;
+
+                    }
+                    
+                }
+                
             }
         }
     }
@@ -74,4 +94,58 @@ public class Shoot : MonoBehaviour
         this.GetComponent<Rigidbody2D>().AddForce(-recoilForce * bulletDirection);
 
     }
+
+    void FireLaserBoostGood()
+    {
+        lastLaserFire = Time.time;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 bulletDirection = worldPosition - transform.position;
+        bulletDirection = new Vector3(bulletDirection.x, bulletDirection.y, 0);
+        Vector2 bulletDirection2D = new Vector2(bulletDirection.x, bulletDirection.y);
+        bulletDirection.Normalize();
+        RaycastHit2D[] laserHits = Physics2D.CapsuleCastAll(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.localScale.x, transform.localScale.y),
+            CapsuleDirection2D.Horizontal, 0, bulletDirection2D, laserLength);
+        foreach (RaycastHit2D collision in laserHits)
+        {
+            if (collision.collider != null && collision.collider.tag == "Enemy")
+            {
+                collision.collider.gameObject.SendMessage("Damage", laserDamage);
+            }
+        }
+        musicManager.RateAction();
+        GetComponent<Parry>().ammo--;
+        Vector3 laserTip = bulletDirection * laserLength;
+        laserTip.z = 6;
+        laser.SetPositions(new Vector3[] { new Vector3(0, 0, 6), laserTip });
+        this.GetComponent<Rigidbody2D>().AddForce(-recoilForce * 3 * bulletDirection);
+
+    }
+
+    void FireLaserBoostPerfect()
+    {
+        lastLaserFire = Time.time;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 bulletDirection = worldPosition - transform.position;
+        bulletDirection = new Vector3(bulletDirection.x, bulletDirection.y, 0);
+        Vector2 bulletDirection2D = new Vector2(bulletDirection.x, bulletDirection.y);
+        bulletDirection.Normalize();
+        RaycastHit2D[] laserHits = Physics2D.CapsuleCastAll(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.localScale.x, transform.localScale.y),
+            CapsuleDirection2D.Horizontal, 0, bulletDirection2D, laserLength);
+        foreach (RaycastHit2D collision in laserHits)
+        {
+            if (collision.collider != null && collision.collider.tag == "Enemy")
+            {
+                collision.collider.gameObject.SendMessage("Damage", laserDamage);
+            }
+        }
+        musicManager.RateAction();
+        GetComponent<Parry>().ammo--;
+        Vector3 laserTip = bulletDirection * laserLength;
+        laserTip.z = 6;
+        laser.SetPositions(new Vector3[] { new Vector3(0, 0, 6), laserTip });
+        this.GetComponent<Rigidbody2D>().AddForce(-recoilForce * 5 * bulletDirection);
+
+    }
+
+
 }
---
+++ Assets/Scripts/finnsihLine.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finnsihLine : MonoBehaviour
+
+     
+{
+    public GameObject player;
+    public float forceAmount = 1;
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            moveAway();
+        }
+    }
+
+    void moveAway()
+    {
+        Vector2 forceToApply;
+        Transform flTransform = this.gameObject.GetComponent<Transform>();
+        Transform playerTransform = player.GetComponent<Transform>();
+        // operator override means we can just subtract directly
+        Vector3 vecToPlayer = playerTransform.position - flTransform.position;
+        // don't care about Z difference
+        forceToApply = new Vector2(vecToPlayer.x, 0);
+        forceToApply.Normalize();
+        forceToApply = forceToApply * forceAmount;
+
+        rb.AddForce(-forceToApply, ForceMode2D.Impulse);
+
+        forceToApply.Normalize();
+        forceToApply = forceToApply * forceAmount;
+
+        rb.AddForce(forceToApply);
+    }
+
+}
