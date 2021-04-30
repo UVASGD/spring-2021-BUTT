ba21bfd47b3024c72400b6861d4ea2172a280ec1
Jav <jt6xnk@virginia.edu>
2021-04-30 16:04:36 -0400

    ok this is epic

---
+++ Assets/Scripts/boss.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss : MonoBehaviour
+{
+
+    public float damage = 1;
+    public GameObject goal;
+    public GameObject manager;
+    public int beatToJump = 1;
+    public GameObject player;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
