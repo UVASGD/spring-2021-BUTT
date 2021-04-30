ddb1ca7127b9e32c527d98db7747b5c49546fca3
GrantMatteo <matteoboy3@gmail.com>
2021-04-30 15:28:43 -0400

    made musicmanager wait for a bit

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -108,7 +108,7 @@ public class MusicManager : MonoBehaviour
         }
         if (curSongTime - initialDelay - beats * betweenBeatDelay >= 0)
         {
-            if ((beats - beatStart) > beatsPerMeasure)
+            if ((beats - beatStart) >= beatsPerMeasure)
             {
                 foreach (GameObject receiver in beatReceivers)
                 {
@@ -130,7 +130,10 @@ public class MusicManager : MonoBehaviour
     }
     void IndicatorOnBeat()
     {
-        GetComponent<Animator>().SetTrigger("Beat");
+        if ((beats - beatStart) >= beatsPerMeasure)
+        {
+            GetComponent<Animator>().SetTrigger("Beat");
+        }
 
 //        GetComponent<SpriteRenderer>().color = black ? new Color(1, 1, 1) : new Color(0, 0, 0);
 //        black = !black;
