7a32b279a32b9b2f5974e73eb18b7dd43bb4be69
Jav <jt6xnk@virginia.edu>
2021-04-30 01:40:51 -0400

    made a boss? stil needs lasers

--- Assets/Scripts/enemy_dos.cs
+++ Assets/Scripts/enemy_dos.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class enemy_dos : MonoBehaviour
 {
     public float forceAmount = 3;
     public GameObject player;
     public float damage = 1;
     private Rigidbody2D rb;
     public Rigidbody2D bullet;
-    public float bulletSpeed = 10;
+    LineRenderer laser;
+    private bool laserFire = false;
+    public float bulletSpeed = 50;
+    public float laserLength = 200;
+    public int laserDamage = 1;
+    public float shootOnBeatLeniency = .06F;
+    public float laserDuration = .1F;
 
     // Start is called before the first frame update
     void Start()
@@ -31,9 +37,19 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void OnBeat(int beatNum)
     {
-        if (beatNum / 4 != 0)
+        
+        if (beatNum / 2 == 0)
         {
-            FireBullet();
+            laserFire = true;
+        }
+        else
+        {
+            laserFire = false;
+        }
+
+        if (laserFire)
+        {
+            FireLaser();
         }
     }
 
@@ -56,5 +72,32 @@ public class NewBehaviourScript : MonoBehaviour
         bulletClone.velocity = bulletSpeed * new Vector2(bulletDirection.x, bulletDirection.y);
 
         
+    }
+    void FireLaser()
+    {
+       // lastLaserFire = Time.time;
+        //Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 bulletDirection = transform.position;
+        bulletDirection = new Vector3(bulletDirection.x, bulletDirection.y, 0);
+        Vector2 bulletDirection2D = new Vector2(bulletDirection.x, bulletDirection.y);
+        bulletDirection.Normalize();
+
+        RaycastHit2D[] laserHits = Physics2D.CapsuleCastAll(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.localScale.x, transform.localScale.y),
+            CapsuleDirection2D.Horizontal, 0, bulletDirection2D, laserLength);
+
+        foreach (RaycastHit2D collision in laserHits)
+        {
+            if (collision.collider != null && collision.collider.tag == "Player")
+            {
+                collision.collider.gameObject.SendMessage("Damage", laserDamage);
+      
+            }
+        }
+        GetComponent<Parry>().ammo--;
+        Vector3 laserTip = bulletDirection * laserLength;
+        laserTip.z = 6;
+        laser.SetPositions(new Vector3[] { new Vector3(0, 0, 6), laserTip });
+        
+
     }
 }
